1ea2e6c597e6e0d6835e37fe0481cfc62da584b3
Trishutrion <146606551+Trishutrion@users.noreply.github.com>
2024-05-29 12:06:10 +1000

    updated menu

---
+++ menu.js
@@ -0,0 +1,8 @@
+let menu = document.getElementById("menu")
+menu.innerHTML = `<a href="index.html">Home</a>
+<a href="video.html">Videos</a>
+<a href="quiz.html">Quizzes</a>
+<a href="game.html">Game</a>
+<a href="personalTracker.html">Personal Tracker</a>
+<a href="contacts.html">Contacts</a>
+`
