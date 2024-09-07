0d13f1917507c7ac2cfbe6929b1c1721eed36f03
Trishutrion <146606551+Trishutrion@users.noreply.github.com>
2024-09-07 15:49:20 +1000

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,15 @@
-/** */
-/** */
-function startGame() {
-    const canvas = document.getElementById("game-canvas")
-    const ctx = canvas.getContext('2d')
-    intro();
-};
-/** */
-function intro() {
+/**
+ * Eco-Solve: Games (Backend)
+ * @author Arnav Bagal
+ * 
+ * @summary This page contains all of the backend code for the Games menu of Eco-Solve.
+ * @description This page contains all of the functions required to run the Games page of Eco-Solve, such as an introduction function, a guide on how to play, a main game loop, and a restart function, for every game on the page. 
+ */
+// Global constants and variables
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext('2d');
+/** Runs the introductory loop for Eco-Quest. */
+function intro() {    
     let running = true;
     canvas.addEventListener('keydown', 
         function (event) {
@@ -33,11 +36,37 @@ function intro() {
         requestAnimationFrame(intro); 
     };    
 };
-/** */
-function howToPlay() {};
+/** Displays instructions on how to play on the screen. */
+function howToPlay() {
+    let running = true;
+    canvas.addEventListener('keydown', 
+        function(event) {
+            if (event.key === 'Enter') {
+                running = false;
+                gameLoop();
+            } else if (event.key == 'b') {
+                running = false;
+                intro();
+            };
+        }
+    );
+    if (running) {
+        `
+        How to Play
+        In Eco-Quest: Part 1, the vat is a blue rectangle and the aliens are red dots.
+        Your aim is to catch as many aliens as you can in 60 seconds. 
+        The vat can be moved using the arrow keys or the WASD keys, 
+        but it cannot leave the game screen.
+        The number of aliens you have collected, as well as the lefover time, 
+        will be displayed on the screen. 
+        So, how many aliens can you catch?
+        Play Eco-Quest: Part 1 to find out!       
+        `
+    };    
+};
 /** */
 function gameLoop() {};
 /** */
 function restart() {};
 // Start the game
-startGame();
+intro();
